5be41fa25363705eabb1ae069b1952dc91c55515
anasrustom <anasrworking@gmail.com>
2024-06-04 18:15:19 +0300

    fixed heatmap

--- Week2Day3PyScript.py
+++ Week2Day3PyScript.py
@@ -3,24 +3,27 @@ import matplotlib.pyplot as plt
 
 # create a 2D array from the csv file that is in C:\Users\anasr\Desktop\Visual studio\QCRI 2024 called gazaDataTest.csv using numpy then make a dataframe from it using pandas
 df = pd.read_csv(r'C:\Users\anasr\Desktop\Visual studio\QCRI 2024\gazeDataTest.csv')
-positive_df = df[(df["FPOGX"] >= 0) & (df["FPOGX"] <= 1) & (df["FPOGY"] >= 0) & (df["FPOGY"] <= 1)]
-gs = positive_df[["FPOGX", "FPOGY"]]
 
-gs["FPOGX"].fillna(gs["FPOGX"].mean(), inplace=True)
-gs["FPOGX"].fillna(gs["FPOGX"].mean(), inplace=True)
-
-
+def clean_data(data):
+    data["FPOGX"].fillna(data["FPOGX"].mean(), inplace=True)    # fill the missing values with the mean of the column
+    data["FPOGX"].fillna(data["FPOGX"].mean(), inplace=True)
+    data = data[(data["FPOGX"] >= 0) & (data["FPOGX"] <= 1) & (data["FPOGY"] >= 0) & (data["FPOGY"] <= 1)] # remove the outliers
+    return data[["FPOGX", "FPOGY"]] # return the cleaned data with only FPOGX and FPOGY columns
 
 def visualizeScatter(data):
     data.plot.scatter(x="FPOGX", y="FPOGY", s=1, c='purple', alpha=0.5) # s is the size of the dots, c is the color, alpha is the transparency
     plt.title("Gaze Data")
     plt.show()
     
-def visualizeHeatMap(data): # this function is not working
-    plt.hist2d(data["FPOGX"], data["FPOGY"], bins=100, cmap='plasma')
+def visualizeHeatMap(data): 
+    plt.hist2d(data["FPOGX"], data["FPOGY"], bins=100, cmap='plasma', vmin=0, vmax=170) # cmap is the color map, vmin and vmax are the minimum and maximum values
     plt.colorbar()
     plt.title("Gaze Data")
+    plt.xlabel("FPOGX")
+    plt.ylabel("FPOGY")
     plt.show()
 
-# visualizeScatter(gs)
+
+gs = clean_data(df)
+visualizeScatter(gs)
 visualizeHeatMap(gs)
