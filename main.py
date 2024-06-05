d99b64c90d74b044af4a226e6e53bb09acee4bdd
anasrustom <anasrworking@gmail.com>
2024-06-05 13:54:56 +0300

    added cvs files and updated main script

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# create a 2D array from the csv file that is in C:\Users\anasr\Desktop\Visual studio\QCRI 2024 called gazaDataTest.csv using numpy then make a dataframe from it using pandas
-df = pd.read_csv(r'/Users/yusufabdelnur/Desktop/QCRI Research/Eye-Tracking Data/User 0_all_gaze - User 0_all_gaze.csv')
+user1 = pd.read_csv('Abdulrahman_all_gaze.csv')
+user2 = pd.read_csv('Howlan_all_gaze.csv')
+user3 = pd.read_csv('Alah_all_gaze.csv')
 
 def clean_data(data):
     # data["FPOGX"].fillna(data["FPOGX"].mean(), inplace=True)    # fill the missing values with the mean of the column?
@@ -16,10 +17,10 @@ def visualizeScatter(data):
     plt.show()
     
 def average_fixation_value(df):
-    print('Averge Fixation Value is', df['FPOGD'].mean())
+    print('Average Fixation Value is', df['FPOGD'].mean())
 
 def visualizeHeatMap(data): 
-    plt.hist2d(data["FPOGX"], data["FPOGY"], bins=100, cmap='plasma', vmin=0, vmax=10) # cmap is the color map, vmin and vmax are the minimum and maximum values
+    plt.hist2d(data["FPOGX"], data["FPOGY"], bins=100, cmap='plasma', vmin=0, vmax=30) # cmap is the color map, vmin and vmax are the minimum and maximum values
     plt.colorbar()
     plt.title("Gaze Data")
     plt.xlabel("FPOGX")
@@ -27,7 +28,23 @@ def visualizeHeatMap(data):
     plt.show()
 
 
-gs = clean_data(df)
+user_number = int(input("Enter user number (1-3): "))
+
+while True:
+    user_number = int(input("Enter user number (1-3): "))
+
+    if user_number == 1:
+        gs = clean_data(user1)
+        break
+    elif user_number == 2:
+        gs = clean_data(user2)
+        break
+    elif user_number == 3:
+        gs = clean_data(user3)
+        break
+    else:
+        print("Invalid user number. Please try again.")
+
 visualizeScatter(gs)
 visualizeHeatMap(gs)
 average_fixation_value(gs)
